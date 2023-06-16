3fb31ec0229fd59ab725883f79ea3e4b27c3bf7e
yurii19 <yurikolechkin@gmail.com>
2023-06-16 12:16:49 +0300

    chore: added ngxs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './components/main/main.component';
+import { NgxsModule } from '@ngxs/store';
+import { CatsService } from './services/cats.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgxsModule.forRoot()
   ],
-  providers: [],
+  providers: [CatsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
