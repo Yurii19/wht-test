afcf6a88c45ece776c4ab296b5fd82009c772429
yurii19 <yurikolechkin@gmail.com>
2023-06-16 15:45:41 +0300

    update: app.state.js

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store, Select } from '@ngxs/store';
+import { Observable, of } from 'rxjs';
 import { CatsService } from 'src/app/services/cats.service';
+import { AppState } from 'src/app/states/app.state';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
-  constructor(private catsService: CatsService) {}
+export class MainComponent implements OnInit {
+  
+  @Select(AppState.selectStateData) cats$: Observable<any> | undefined ;
+
+  constructor(private catsService: CatsService, private store: Store) {
+    //this.cats$ =
+  }
+
+  ngOnInit(): void {
+    // this.cats$ = this.store.select('cats');
+    // this.cats$.subscribe(d =>{
+    //   console.log(d)
+    // })
+  }
   getCats() {
     this.catsService.fetchCats().subscribe((d) => {
       console.log(d);
