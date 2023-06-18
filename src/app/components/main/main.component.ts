5092267e9e287912cb11b6753b00485f4fa9f935
yurii19 <yurikolechkin@gmail.com>
2023-06-18 17:39:01 +0300

    chore: gradient changed

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store, Select } from '@ngxs/store';
-import { Observable, Subject, debounceTime, filter, takeUntil } from 'rxjs';
 import {
-  GetBreeds,
-  GetCatsWithFilter,
-} from 'src/app/actions/app.actions';
+  Observable,
+  Subject,
+  debounceTime,
+  filter,
+  skip,
+  takeUntil,
+} from 'rxjs';
+import { GetBreeds, GetCatsWithFilter } from 'src/app/actions/app.actions';
 import { AppState } from 'src/app/states/app.state';
 import { FormControl, FormGroup } from '@angular/forms';
 import { IFilter } from 'src/app/types';
@@ -19,7 +23,7 @@ export class MainComponent implements OnInit, OnDestroy {
   @Select(AppState.selectStateBreeds) breeds$: Observable<any> | undefined;
 
   filters: FormGroup = new FormGroup({
-    limit: new FormControl(2),
+    limit: new FormControl(4),
     breeds: new FormControl(),
   });
 
@@ -31,12 +35,13 @@ export class MainComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.store.dispatch(new GetCatsWithFilter(this.filters.value));
     this.store.dispatch(new GetBreeds());
-    this.breeds$?.subscribe((breeds) =>
-      this.filters.get('breeds')?.setValue(breeds[0])
-    );
+    this.breeds$
+      ?.pipe(takeUntil(this.destroy$))
+      .subscribe((breeds) => this.filters.get('breeds')?.setValue(breeds[0]));
 
     this.filters?.valueChanges
       .pipe(
+        skip(1),
         takeUntil(this.destroy$),
         debounceTime(500),
         filter((formFilter: IFilter) => formFilter.limit !== null)
